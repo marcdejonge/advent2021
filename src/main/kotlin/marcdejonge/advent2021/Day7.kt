753ced01452a02c1e962558a0951e9954d0e7a8d
Marc de Jonge <marc.dejonge@jamf.com>
2021-12-07 12:54:25 +0100

    Improve day 7 readability and don't let the positions be fixed by me

--- src/main/kotlin/marcdejonge/advent2021/Day7.kt
+++ src/main/kotlin/marcdejonge/advent2021/Day7.kt
@@ -5,15 +5,16 @@ import kotlin.math.absoluteValue
 fun main() = Day7.printSolution()
 
 object Day7 : DaySolver(7) {
-    private val crabPositions = input.first().split(",").map { it.toInt() }
+    private val crabPositions = input.first().split(",").map { it.toInt() }.toIntArray()
+    private val possiblePositions = crabPositions.minOf { it }..crabPositions.maxOf { it }
 
     private val Int.triangle get() = this * (this + 1) / 2
 
-    override fun calcPart1() = (0..1000).minOf { pos ->
-        crabPositions.sumOf { (it - pos).absoluteValue }
+    override fun calcPart1() = possiblePositions.minOf { position ->
+        crabPositions.sumOf { (it - position).absoluteValue }
     }
 
-    override fun calcPart2() = (0..1000).minOf { pos ->
-        crabPositions.sumOf { (it - pos).absoluteValue.triangle }
+    override fun calcPart2() = possiblePositions.minOf { position ->
+        crabPositions.sumOf { (it - position).absoluteValue.triangle }
     }
 }
