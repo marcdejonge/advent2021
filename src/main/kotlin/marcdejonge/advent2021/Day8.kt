fbb4ff3fb1a28d08c7f681d2b0a160b613265ad8
Marc de Jonge <marc.dejonge@jamf.com>
2021-12-08 11:04:38 +0100

    Make Day 8 more readable

--- src/main/kotlin/marcdejonge/advent2021/Day8.kt
+++ src/main/kotlin/marcdejonge/advent2021/Day8.kt
@@ -10,32 +10,30 @@ object Day8 : DaySolver(8) {
 
     private val lines = input.map { line ->
         line.split(" | ").map {
-            it.split(" ").map { digit -> digit.parseInputWires() }
+            it.split(" ").map { digit -> digit.parseInputWires() }.asSequence()
         }
     }
 
-    private fun parseWires(digits: List<Int>): Map<Int, Int> {
-        val n1 = digits.single { it.countOneBits() == 2 }
-        val n7 = digits.single { it.countOneBits() == 3 }
-        val n4 = digits.single { it.countOneBits() == 4 }
-        val n8 = digits.single { it.countOneBits() == 7 }
+    private fun Sequence<Int>.findDigit(segments: Int, filter: (Int) -> Boolean = { true }) =
+        filter { it.countOneBits() == segments }.single { filter(it) }
 
-        val n3 = digits.single { it.countOneBits() == 5 && (it and n1) == n1 }
-        val leftTopBar = n4 xor (n3 and n4)
-        val n5 = digits.single { it.countOneBits() == 5 && (it and leftTopBar) != 0 }
-        val rightLowBar = n5 and n1
-        val n2 = digits.single { it.countOneBits() == 5 && (it and rightLowBar) == 0 }
-        val rightTopBar = n1 xor rightLowBar
-        val n6 = digits.single { it.countOneBits() == 6 && (it and rightTopBar) == 0 }
-        val leftLowBar = n6 xor n5
-        val n9 = digits.single { it.countOneBits() == 6 && (it and leftLowBar) == 0 }
-        val n0 = digits.single { it != n6 && it != n9 && it.countOneBits() == 6 }
+    private fun parseWires(digits: Sequence<Int>): Map<Int, Int> {
+        val n1 = digits.findDigit(segments = 2)
+        val n7 = digits.findDigit(segments = 3)
+        val n4 = digits.findDigit(segments = 4)
+        val n8 = digits.findDigit(segments = 7)
+        val n9 = digits.findDigit(segments = 6) { (it and n4) == n4 }
+        val n0 = digits.findDigit(segments = 6) { it != n9 && (it and n1) == n1 }
+        val n6 = digits.findDigit(segments = 6) { it != n9 && it != n0 }
+        val n3 = digits.findDigit(segments = 5) { (it and n1) == n1 }
+        val n5 = digits.findDigit(segments = 5) { it != n3 && (it and n9) == it }
+        val n2 = digits.findDigit(segments = 5) { it != n3 && it != n5 }
 
         return mapOf(n0 to 0, n1 to 1, n2 to 2, n3 to 3, n4 to 4, n5 to 5, n6 to 6, n7 to 7, n8 to 8, n9 to 9)
     }
 
-    private fun Map<Int, Int>.parseDigits(digits: List<Int>) =
-        digits.asSequence().map { this[it]!! }.fold(0) { curr, n -> curr * 10 + n }
+    private fun Map<Int, Int>.parseDigits(digits: Sequence<Int>) =
+        digits.map { this[it]!! }.fold(0) { curr, n -> curr * 10 + n }
 
     override fun calcPart1() = lines.sumOf { (_, digits) ->
         digits.count { it.countOneBits() in 2..4 || it.countOneBits() == 7 }
